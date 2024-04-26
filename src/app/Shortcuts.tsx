3fca5380bbc4377e9a35c752e4c573fcd3b364e9
ronnie <rondinelibrt18@gmail.com>
2024-04-26 14:06:18 -0300

    fix: tab issue

--- src/app/Shortcuts.tsx
+++ src/app/Shortcuts.tsx
@@ -6,7 +6,10 @@ const useShortcuts = (
   currentTab,
   setTabs,
   tabText,
-  textArea: { current: HTMLTextAreaElement }
+  textArea: { current: HTMLTextAreaElement },
+  setShowEmojis,
+  showEmojis,
+  emojilist
 ) => {
   const [cache, setCache] = useState(tabText);
 
@@ -54,143 +57,184 @@ const useShortcuts = (
     return { lineStart, lineEnd };
   }
 
-  useEffect(() => {
-    let ctrlPressed = false;
-    let shiftPressed = false;
-    let altPressed = false;
+  let ctrlPressed = false;
+  let shiftPressed = false;
+  let altPressed = false;
 
-    const keydown = document.addEventListener("keydown", (e) => {
-      if (e.ctrlKey) {
-        ctrlPressed = true;
-      }
+  function handleKeyDown(e) {
+    if (e.ctrlKey) {
+      ctrlPressed = true;
+    }
 
-      if (e.shiftKey) {
-        shiftPressed = true;
-      }
+    if (e.shiftKey) {
+      shiftPressed = true;
+    }
 
-      if (e.altKey) {
-        altPressed = true;
-      }
+    if (e.altKey) {
+      altPressed = true;
+    }
 
-      // COMMANDS
+    // COMMANDS
 
-      if (ctrlPressed && e.key == 0) {
-        e.preventDefault();
-        formatInfo();
-      }
+    if (ctrlPressed && e.key == 0) {
+      e.preventDefault();
+      formatInfo();
+    }
 
-      if (ctrlPressed && shiftPressed && e.key === ":") {
-        e.preventDefault();
+    if (ctrlPressed && e.key == "]") {
+      e.preventDefault();
 
-        if (textArea.current) {
-          const cursorPos = textArea.current.selectionStart;
+      setShowEmojis((state) => !state);
+    }
 
-          setTabs((tabs) => {
-            const updatedTabs = [...tabs];
-            const { text } = updatedTabs[currentTab];
+    if (ctrlPressed && shiftPressed && e.key === ":") {
+      e.preventDefault();
 
-            const { lineStart, lineEnd } = findLineBounds(text, cursorPos);
-
-            // Adicionar "> " ao início da linha atual
-            const updatedText =
-              text.substring(0, lineStart) +
-              "> " +
-              text.substring(lineStart, lineEnd) +
-              text.substring(lineEnd);
-
-            // Atualizar o texto no array de tabs
-            updatedTabs[currentTab].text = updatedText;
-
-            return updatedTabs;
-          });
-
-          // Restaurar a posição do cursor após a atualização do estado
-          setTimeout(() => {
-            if (textArea.current) {
-              textArea.current.setSelectionRange(cursorPos + 2, cursorPos + 2); // Adiciona 2 para considerar "> "
-            }
-          }, 0); // Usamos um pequeno atraso para garantir que a atualização do estado tenha sido concluída
-        }
-      }
-
-      if (altPressed && (e.key == "ArrowDown" || e.key == "ArrowUp")) {
-        e.preventDefault();
+      if (textArea.current) {
         const cursorPos = textArea.current.selectionStart;
-        try {
-          setTabs((tabs) => {
-            const updatedTabs = [...tabs];
-            const { text } = updatedTabs[currentTab];
-            const textLines = text.split("\n");
 
-            const { lineStart, lineEnd } = findLineBounds(text, cursorPos);
-
-            let nlCount = text.substring(0, lineEnd).split("\n").length - 1;
-
-            if (e.key == "ArrowUp" && nlCount > 0) {
-              let nl = textLines[nlCount];
-              textLines[nlCount] = textLines[nlCount - 1];
-              textLines[nlCount - 1] = nl;
-
-              // Atualizar o texto no array de tabs
-              updatedTabs[currentTab].text = textLines.join("\n");
-
-              // Restaurar a posição do cursor após a atualização do estado
-              setTimeout(() => {
-                const newCursorPos = cursorPos - textLines[nlCount].length - 1;
-                textArea.current.setSelectionRange(newCursorPos, newCursorPos);
-              }, 0);
-            }
-
-            if (e.key == "ArrowDown" && nlCount < textLines.length - 1) {
-              let nl = textLines[nlCount];
-              textLines[nlCount] = textLines[nlCount + 1];
-              textLines[nlCount + 1] = nl;
-
-              // Atualizar o texto no array de tabs
-              updatedTabs[currentTab].text = textLines.join("\n");
-
-              // Restaurar a posição do cursor após a atualização do estado
-              setTimeout(() => {
-                const newCursorPos = cursorPos + textLines[nlCount].length + 1;
-                textArea.current.setSelectionRange(newCursorPos, newCursorPos);
-              }, 0);
-            }
-
-            return updatedTabs;
-          });
-        } catch (e) {
-          console.log("unable to change lines");
-        }
-      }
-
-      if (ctrlPressed && e.key == "z") {
         setTabs((tabs) => {
           const updatedTabs = [...tabs];
-          updatedTabs[currentTab].text = cache;
+          const { text } = updatedTabs[currentTab];
+
+          const { lineStart, lineEnd } = findLineBounds(text, cursorPos);
+
+          // Adicionar "> " ao início da linha atual
+          const updatedText =
+            text.substring(0, lineStart) +
+            "> " +
+            text.substring(lineStart, lineEnd) +
+            text.substring(lineEnd);
+
+          // Atualizar o texto no array de tabs
+          updatedTabs[currentTab].text = updatedText;
+
           return updatedTabs;
         });
-      }
-    });
 
-    const keyUp = document.addEventListener("keyup", (e) => {
-      if (e.ctrlKey) {
-        ctrlPressed = false;
+        // Restaurar a posição do cursor após a atualização do estado
+        setTimeout(() => {
+          if (textArea.current) {
+            textArea.current.setSelectionRange(cursorPos + 2, cursorPos + 2); // Adiciona 2 para considerar "> "
+          }
+        }, 0); // Usamos um pequeno atraso para garantir que a atualização do estado tenha sido concluída
       }
+    }
 
-      if (e.shiftKey) {
-        shiftPressed = false;
-      }
+    if (altPressed && (e.key == "ArrowDown" || e.key == "ArrowUp")) {
+      e.preventDefault();
+      const cursorPos = textArea.current.selectionStart;
+      try {
+        setTabs((tabs) => {
+          const updatedTabs = [...tabs];
+          const { text } = updatedTabs[currentTab];
+          const textLines = text.split("\n");
 
-      if (e.altKey) {
-        altPressed = false;
+          const { lineStart, lineEnd } = findLineBounds(text, cursorPos);
+
+          let nlCount = text.substring(0, lineEnd).split("\n").length - 1;
+
+          if (e.key == "ArrowUp" && nlCount > 0) {
+            let nl = textLines[nlCount];
+            textLines[nlCount] = textLines[nlCount - 1];
+            textLines[nlCount - 1] = nl;
+
+            // Atualizar o texto no array de tabs
+            updatedTabs[currentTab].text = textLines.join("\n");
+
+            // Restaurar a posição do cursor após a atualização do estado
+            setTimeout(() => {
+              const newCursorPos = cursorPos - textLines[nlCount].length - 1;
+              textArea.current.setSelectionRange(newCursorPos, newCursorPos);
+            }, 0);
+          }
+
+          if (e.key == "ArrowDown" && nlCount < textLines.length - 1) {
+            let nl = textLines[nlCount];
+            textLines[nlCount] = textLines[nlCount + 1];
+            textLines[nlCount + 1] = nl;
+
+            // Atualizar o texto no array de tabs
+            updatedTabs[currentTab].text = textLines.join("\n");
+
+            // Restaurar a posição do cursor após a atualização do estado
+            setTimeout(() => {
+              const newCursorPos = cursorPos + textLines[nlCount].length + 1;
+              textArea.current.setSelectionRange(newCursorPos, newCursorPos);
+            }, 0);
+          }
+
+          return updatedTabs;
+        });
+      } catch (e) {
+        console.log("unable to change lines");
       }
-    });
+    }
+
+    if (ctrlPressed && e.key == "z") {
+      setTabs((tabs) => {
+        const updatedTabs = [...tabs];
+        updatedTabs[currentTab].text = cache;
+        return updatedTabs;
+      });
+    }
+  }
+
+  function handleKeyUp(e) {
+    if (e.ctrlKey) {
+      ctrlPressed = false;
+    }
+
+    if (e.shiftKey) {
+      shiftPressed = false;
+    }
+
+    if (e.altKey) {
+      altPressed = false;
+    }
+  }
+
+  useEffect(() => {
+    const keydown = document.addEventListener("keydown", handleKeyDown);
+
+    const keyUp = document.addEventListener("keyup", handleKeyUp);
 
     return () => {
-      document.removeEventListener("keydown", keydown);
-      document.removeEventListener("keyup", keyUp);
+      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("keyup", handleKeyUp);
     };
   }, [currentTab]);
+
+  function handleKeyDownEmojis(e) {
+    if (showEmojis && "0123456789".includes(e.key)) {
+      e.preventDefault();
+      const cursorPos = textArea.current.selectionStart;
+      const emoji = emojilist[e.key];
+
+      setTabs((tabs) => {
+        const updatedTabs = [...tabs];
+        const { text } = updatedTabs[currentTab];
+
+        updatedTabs[currentTab].text =
+          text.substring(0, cursorPos) + emoji + text.substring(cursorPos);
+
+        return updatedTabs;
+      });
+      setTimeout(() => {
+        if (textArea.current) {
+          textArea.current.setSelectionRange(cursorPos + 1, cursorPos + 1);
+        }
+      }, 0);
+    }
+  }
+
+  useEffect(() => {
+    const keydown = document.addEventListener("keydown", handleKeyDownEmojis);
+  }, [showEmojis]);
+
+  return () => {
+    document.removeEventListener("keydown", handleKeyDownEmojis);
+  };
 };
 
 export default useShortcuts;
